5598f7f6d56e7ee295f4782eb80779cd2efff2e9
6kun <40051635+6kun@users.noreply.github.com>
2025-03-02 14:14:10 +0800

    Update groups.ts

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -11,13 +11,13 @@ export const groups: Group[] = [
   {
     id: 'group1',
     name: '写作交流群',
-    description: '',
-    members: [ 'ai4', 'ai5', 'ai6', 'ai7', 'ai8'],
+    description: '多人配合，按照自己的职责，创作一部小说',
+    members: [ 'ai0', 'ai4', 'ai5', 'ai6', 'ai7', 'ai8'],
     isGroupDiscussionMode: false
   },
   {
     id: 'group2',
-    name: 'AI成语接龙游戏群',
+    name: '成语接龙游戏',
     description: '可以适当打招呼问候自我介绍，但是本群主线是成语接龙游戏，请严格按照文字成语接龙规则，不能过度闲聊',
     isGroupDiscussionMode: true,
     members: [ 'ai8', 'ai4', 'ai5', 'ai6', 'ai7'],
