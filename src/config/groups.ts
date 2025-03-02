03fff978267079be465e4cf9f3b85a045692b3a9
6kun <40051635+6kun@users.noreply.github.com>
2025-03-02 17:10:23 +0800

    Update groups.ts

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -12,7 +12,7 @@ export const groups: Group[] = [
     id: 'group1',
     name: '写作交流群',
     description: '多人配合，按照自己的职责，创作一部小说',
-    members: [ 'ai7', 'ai4', 'ai5', 'ai6', 'ai8', 'ai3'],
+    members: [ 'ai7', 'ai1', 'ai4', 'ai5', 'ai2', 'ai6', 'ai8', 'ai3'],
     isGroupDiscussionMode: false
   },
   {
